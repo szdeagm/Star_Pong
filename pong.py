02072f8bfbd6e5a3dd2d6ce306cd33ced84bcd3a
mari <souzadeaguiar.ma@gmail.com>
2026-06-21 20:46:46 -0300

    detecção das barras

--- pong.py
+++ pong.py
@@ -1,6 +1,9 @@
 from turtle import *
 import random
 
+jogador1 = 0
+jogador2 = 0
+
 def tela():
     title("Star Pong")
     setup(width = 800, height = 600)
@@ -44,14 +47,14 @@ darth.penup()
 darth.speed(0)
 darth.goto(360, 0) 
 
+p = Turtle()
 def placar():
-    p = Turtle()
     p.hideturtle()
     p.color("white")
     p.penup()
     p.goto(-250, -285)
     p.speed(0)
-    p.write("Jogador 1: 0 | Jogador 2: 0", font = ("Courrier", 13, "bold"), align = "center")
+    p.write(f"Jogador 1: {jogador1}  |  Jogador 2: {jogador2}", font = ("Courrier", 13, "bold"), align = "center")
     
 b = Turtle()
 def bola():
@@ -59,17 +62,53 @@ def bola():
     b.color("pink")
     b.penup()
     b.goto(0,0)
-    b.dx = 0
-    b.dy = 0
+    b.dx = 6
+    b.dy = 6
+
+def bola_mov():
+    b.setx(b.xcor() + b.dx)
+    b.sety(b.ycor() + b.dy)
+    global jogador1, jogador2
+
+    if b.ycor() > 250:
+        b.sety(250)
+        b.dy *= -1
+    if b.ycor() < -250:
+        b.sety(-250)
+        b.dy *= -1
+    if b.xcor() > 370:
+        b.goto(0,0)
+        b.dx *= -1
+        jogador1 += 1
+        p.clear()
+        p.write(f"Jogador 1:{jogador1}  | Jogador 2: {jogador2}", font = ("Courrier", 13, "bold"), align = "center")
+    if b.xcor() < -370:
+        b.goto(0,0)
+        b.dx *= -1
+        jogador2 += 1
+        p.clear()
+        p.write(f"Jogador 1:{jogador1}  | Jogador 2: {jogador2}", font = ("Courrier", 13, "bold"), align = "center")
+    if (b.xcor() < -350 and b.xcor() > -370) and (b.ycor() < tluke.ycor() + 80 and b.ycor() > tluke.ycor() - 80):
+        b.setx(370)
+        b.dx *= -1
+    if (b.xcor() < 350 and b.xcor() > 370) and (b.ycor() < darth.ycor() + 80 and b.ycor() > darth.ycor() - 80):
+        b.setx(-370)
+        b.dx *= -1
+
+    update()
+    ontimer(bola_mov, 10)
 
 tela()
 placar()
 bola()
-
 listen()
 
 #teclas
 onkeypress(luke_up, "Up")
 onkeypress(luke_down, "Down")
+
+#loop do jogo
+
+bola_mov()   
 done()
 
