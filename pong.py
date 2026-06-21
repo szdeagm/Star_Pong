2665829490e27734c0018c8bc98f420b41e1e2aa
mari <souzadeaguiar.ma@gmail.com>
2026-06-21 19:02:21 -0300

    bola e placar

--- pong.py
+++ pong.py
@@ -1,11 +1,11 @@
 from turtle import *
-
+import random
 
 def tela():
     title("Star Pong")
     setup(width = 800, height = 600)
     bgcolor("black")
-    goto(-380, -280)
+    goto(-380, -250)
     hideturtle()
     speed(0)
     width(3)
@@ -13,7 +13,7 @@ def tela():
     for i in range(2):
         forward(760)
         left(90)
-        forward(560)
+        forward(500)
         left(90)
 
 tluke = Turtle()
@@ -27,12 +27,12 @@ tluke.goto(-360, 0)
 
 def luke_up():
     y = tluke.ycor()
-    if y < 190:
+    if y < 160:
         tluke.sety(y + 20)
 
 def luke_down():
     y = tluke.ycor()
-    if y > -190:
+    if y > -160:
         tluke.sety(y - 20)
 
 darth = Turtle()
@@ -44,8 +44,31 @@ darth.penup()
 darth.speed(0)
 darth.goto(360, 0) 
 
+def placar():
+    p = Turtle()
+    p.hideturtle()
+    p.color("white")
+    p.penup()
+    p.goto(-250, -285)
+    p.speed(0)
+    p.write("Jogador 1: 0 | Jogador 2: 0", font = ("Courrier", 13, "bold"), align = "center")
+    
+b = Turtle()
+def bola():
+    b.shape("circle")
+    b.color("pink")
+    b.penup()
+    b.goto(0,0)
+    b.dx = 0
+    b.dy = 0
+
 tela()
+placar()
+bola()
+
 listen()
+
+#teclas
 onkeypress(luke_up, "Up")
 onkeypress(luke_down, "Down")
 done()
